30b9a90435ec57447b3598ddcd55e732af244dac
Habibur Rehman Mansuri <56839228+HabiburRehman05@users.noreply.github.com>
2023-11-15 23:29:45 +0530

    Delete Table_Management directory

--- Table_Management/src/main/java/com/example/exception/TableAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.exception;
-
-public class TableAlreadyExistsException extends RuntimeException{
-	
-	public TableAlreadyExistsException(String message) {
-        super(message);
-    }
-}
